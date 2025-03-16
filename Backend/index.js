26a6f2cf4f94ef4be9dabb2cffc161609ef1df7a
“buiquocdat30” <buiquocdat30@gmail.com>
2025-03-16 20:25:55 +0700

    BE Day 3 16032025 20h25 PM

--- Backend/index.js
+++ Backend/index.js
@@ -5,25 +5,50 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
-const cors=require('cors')
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
 
 //Database Connection with MongoDB
-mongoose.connect("mongodb+srv://buiquocdat30:DAT0369948689@shopper.saapn.mongodb.net/Ecommerce")
+mongoose.connect(
+  "mongodb+srv://buiquocdat30:DAT0369948689@shopper.saapn.mongodb.net/Ecommerce"
+);
 
 //API creattion
 
-app.get("/",(req, res)=>{
-    res.send("Express App is Running")
-})
+app.get("/", (req, res) => {
+  res.send("Express App is Running");
+});
 
-app.listen(port,(error)=>{
-    if(!error){
-        console.log('Server running on port: '+port)
-    }
-    else{
-        console.log("Error:"+error)
-    }
-})
+// Image Storage Engine
+
+const storage = multer.diskStorage({
+  destination: "/upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// Creating Upload Endpoint for images
+app.use("/images", express.static("upload/images"));
+
+app.port("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
+});
+
+app.listen(port, (error) => {
+  if (!error) {
+    console.log("Server running on port: " + port);
+  } else {
+    console.log("Error:" + error);
+  }
+});
