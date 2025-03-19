005bd3e94c07f256f0e84df8e8b5e5c4c36dd60b
buiquocdat30 <buiquocdat30@gmail.com>
2025-03-19 10:53:08 +0700

    Day 6 19032025 10h54 AM

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
-import './AddProduct.css'
-const AddProduct = () => {
-  return (
-    <div className='add-product'></div>
-  )
-}
+import React, { useState } from "react";
+import "./AddProduct.css";
+import upload_area from "../../assets/upload_area.svg";
 
-export default AddProduct
+const AddProduct = () => {
+  const [image, setImage] = useState(false);
+  const imageHandler=(e)=>{
+    const file = e.target.files[0];
+    if (file) {
+        const imageUrl = URL.createObjectURL(file);
+        console.log("Image URL:", imageUrl);
+        setImage(file);
+    }
+  }
+
+
+  return (
+    <div className="add-product">
+      <div className="addproduct-itemfield">
+        <p>Product Title</p>
+        <input type="text" name="name" placeholder="Type here" />
+      </div>
+      <div className="addproduct-price">
+        <div className="addproduct-itemfield">
+          <p>Price</p>
+          <input type="text" name="old_name" placeholder="Type here" />
+        </div>
+        <div className="addproduct-itemfield">
+          <p>Offer Price</p>
+          <input type="text" name="new_name" placeholder="Type here" />
+        </div>
+      </div>
+      <div className="addproduct-itemfield">
+        <p>Product Category</p>
+        <select name="category" className="add-product-selector">
+          <option value="women">Women</option>
+          <option value="men">Men</option>
+          <option value="kid">Kid</option>
+        </select>
+      </div>
+      <div className="addproduct-itemfield">
+        <label htmlFor="file-input">
+          {/* Nếu image có giá trị (tức là có ảnh được chọn), thì dùng URL.createObjectURL(image) để tạo URL tạm thời hiển thị ảnh đó còn không thì hiển thị hình ảnh mặc định từ upload_area */}
+          <img className="addproduct-thumnail-img" src={image?URL.createObjectURL(image):upload_area} alt="" />
+        </label>
+        <input onChange={imageHandler} type="file" name="image" id="file-input" hidden />
+      </div>
+      <button className="addproduct-btn">ADD</button>
+    </div>
+  );
+};
+
+export default AddProduct;
