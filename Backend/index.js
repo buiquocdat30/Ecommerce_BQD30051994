e2f73de171bd6f366fd6d1775ef0c9cda4e0e53b
buiquocdat30 <buiquocdat30@gmail.com>
2025-03-22 16:31:16 +0700

    Day 9 22032025 16h31 PM

--- Backend/index.js
+++ Backend/index.js
@@ -174,37 +174,58 @@ const Users = mongoose.model("Users", {
 });
 
 // Creating Endpoint for registering the User
-app.post("/singup", async (req, res) => {
+app.post("/signup", async (req, res) => {
   let check = await Users.findOne({ email: req.body.email });
   if (check) {
-    return res
-      .status(400)
-      .json({
-        success: false,
-        error: "existing user found with same email address",
-      });
+    return res.status(400).json({
+      success: false,
+      error: "existing user found with same email address",
+    });
   }
   let cart = {};
   for (let i = 0; i < 300; i++) {
     cart[i] = 0;
   }
-  const user=new Users({
-    name:req.body.username,
-    email:req.body.email,
-    password:req.body.password,
-    cartData:cart,
-  })
+  const user = new Users({
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    cartData: cart,
+  });
 
   await user.save();
 
-  const data={
-    user:{
-      id:user.id
+  const data = {
+    user: {
+      id: user.id,
+    },
+  };
+
+  const token = jwt.sign(data, "secret_ecom");
+  res.json({ success: true, token });
+});
+
+//Creating endpoint for user login
+app.post("/login", async (req, res) => {
+  let user = await Users.findOne({ email: req.body.email });
+  if (user) {
+    const passCompare = req.body.password === user.password;
+    if (passCompare) {
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = jwt.sign(data, "secret_ecom");
+      res.json({ success: true, token });
+    }
+    else{
+      res.json({ success: false, error:"Wrong Password" })
     }
   }
-
-  const token= jwt.sign(data,'secret_ecom');
-  res.json({success:true,token})
+  else{
+    res.json({ success: false, error:"Wrong Email Id" })
+  }
 });
 
 app.listen(port, (error) => {
